1039e185aa2a906370db822fe6ed3554e03e7eaf
EzequielCaballero <ezequiel.caballero33@gmail.com>
2020-04-18 19:20:37 -0300

    Navbar - new component

---
+++ src/frontend/components/navbar/navbar.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+//CSS
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="nav-custom">
+      <input type="checkbox" id="nav-custom-check" />
+      <div className="nav-custom-header">
+        <div className="nav-custom-title">
+          <a href="/home" rel="noopener noreferrer">
+            Inicio
+          </a>
+        </div>
+      </div>
+      <div className="nav-custom-btn">
+        <label htmlFor="nav-custom-check">
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+      </div>
+
+      <div className="nav-custom-links">
+        <a href="/calculator" rel="noopener noreferrer">
+          Calculadora
+        </a>
+        <a href="/" rel="noopener noreferrer">
+          ¿Cómo funciona?
+        </a>
+        <a href="/" rel="noopener noreferrer">
+          Contacto
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
