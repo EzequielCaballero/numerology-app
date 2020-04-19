8ad773115f87b011d0ab6c55e380b3300a0c7af2
EzequielCaballero <ezequiel.caballero33@gmail.com>
2020-04-19 14:28:31 -0300

    Footer - new component

---
+++ src/frontend/components/footer/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+//CSS
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-box">
+      <p className="footer-title">
+        Creado por <strong>Ezequiel Caballero</strong>
+        <span> | 2020</span>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
