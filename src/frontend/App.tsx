6f1e5a7666682491002d4d18f321542a0927bc98
EzequielCaballero <ezequiel.caballero33@gmail.com>
2020-08-22 14:37:58 -0300

    Using imgLoader component

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -3,29 +3,29 @@ import { Route, Switch } from 'react-router-dom';
 //COMPONENTS
 import Navbar from './components/navbar/navbar';
 import Footer from './components/footer/footer';
-import { Routes } from '../backend/sitemap/routes';
 import Err404 from './components/err404/err404';
+import ImgLoader from './components/img/loader';
+import Routes from '../backend/sitemap/routes';
 //BASIC STYLE
+import _backgroundHD from './assets/background-hd.jpg';
+import _backgroundLD from './assets/background-ld.jpg';
 import './App-variables.css';
 import './App-style.css';
 import './App-responsive.css';
 import './App-animation.css';
 
-function App() {
+const App = (): JSX.Element => {
+	const imgHDLoaded = ImgLoader(_backgroundHD);
+
 	return (
-		<div className="site">
+		<div className="site" style={{ backgroundImage: `url(${imgHDLoaded || _backgroundLD})` }}>
 			<header className="site-header">
 				<Navbar />
 			</header>
 			<main className="site-content">
 				<Switch>
-					{Routes.map(({ key, path, exact, component: Component, ...rest }) => (
-						<Route
-							key={key}
-							path={path}
-							exact={exact}
-							render={(props) => <Component {...props} {...rest} />}
-						/>
+					{Routes.map(({ key, path, exact, component: Component }) => (
+						<Route key={key} path={path} exact={exact} render={(props) => <Component {...props} />} />
 					))}
 					<Route render={(props) => <Err404 {...props} />} />
 				</Switch>
@@ -35,6 +35,6 @@ function App() {
 			</footer>
 		</div>
 	);
-}
+};
 
 export default App;
