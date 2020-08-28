ee4e85c393e3c745b38ce5dc82cbe036476ce58f
EzequielCaballero <ezequiel.caballero33@gmail.com>
2020-08-27 22:16:53 -0300

    New component to handle head index.html and theme

--- src/frontend/components/head/head.tsx
+++ src/frontend/components/head/head.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import useImgLoader from '../../hook/img/loader';
+import { useImgLoader } from '../../hooks/img/loader';
+import { useSetupContext } from '../../provider/setup';
 import _backgroundX4 from '../../assets/background-x4.jpg';
 import _backgroundX3 from '../../assets/background-x3.jpg';
 import _backgroundX2 from '../../assets/background-x2.jpg';
@@ -9,15 +10,17 @@ import _backgroundPH from '../../assets/background-ph.jpg';
 
 const Head = () => {
 	const srcs: string[] = [ _backgroundX4, _backgroundX3, _backgroundX2, _backgroundX1 ];
-	const backgroundImg = useImgLoader(srcs, _backgroundPH);
+	const { theme } = useSetupContext();
+	const background = useImgLoader(srcs, _backgroundPH);
+
 	return (
 		<Helmet>
-			<html lang="en" />
+			<html lang="es" />
 			<meta name="description" content="Calculadora para la obtención de números pitagóricos" />
-			<meta name="theme-color" content="#342042" />
-			<title>Numerología pitagórica con React</title>
-			<body data-theme={'dark'} />
-			<style type="text/css">{`.site { background-image: url(${backgroundImg}) }`}</style>
+			<meta name="theme-color" content={theme === 'dark' ? '#000000' : '#ffffff'} />
+			<title>Numerología pitagórica</title>
+			<body data-theme={theme} />
+			<style type="text/css">{`.site { background-image: url(${background}) }`}</style>
 		</Helmet>
 	);
 };
