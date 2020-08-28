ae534728fe151ac2348afa42b915d8bd6a984b41
EzequielCaballero <ezequiel.caballero33@gmail.com>
2020-08-27 22:13:23 -0300

    New global provider for user preferences (wip)

---
+++ src/frontend/provider/setup.tsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect, useContext } from 'react';
+
+enum Theme {
+	Dark = 'dark',
+	Light = 'light'
+}
+
+type ContextProps = {
+	theme: Theme;
+	switchTheme: () => void;
+};
+
+const ContextSetup = React.createContext<ContextProps>(undefined!);
+
+export const useSetupContext = () => useContext(ContextSetup);
+
+export const ProviderSetup: React.FunctionComponent = ({ children }) => {
+	const [ theme, setTheme ] = useState<Theme>(Theme.Dark);
+
+	const switchTheme = () => {
+		setTheme(theme === Theme.Dark ? Theme.Light : Theme.Dark);
+	};
+
+	useEffect(
+		() => {
+			if (theme === undefined) {
+				setTheme(Theme.Dark);
+			}
+		},
+		[ theme ]
+	);
+
+	return <ContextSetup.Provider value={{ theme, switchTheme }}>{children}</ContextSetup.Provider>;
+};
