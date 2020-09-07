8ba122b7ccadd7c49a7044df356a6a1c7575b3c4
EzequielCaballero <ezequiel.caballero33@gmail.com>
2020-09-07 01:53:43 -0300

    Minor style fixes

--- src/client/front/components/cross/navbar/option/lang.tsx
+++ src/client/front/components/cross/navbar/option/lang.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useContextSetup } from '../../../../context/setup';
 import { Switcher } from '../../../switcher/switcher';
 
 export const NavbarLang: React.FunctionComponent = () => {
 	const { translate, switchLang } = useContextSetup();
 	const langSwitch = translate.locale() === 'en' ? 'ES' : 'EN';
+
+	useEffect(
+		() => {
+			if (langSwitch === 'ES') {
+				const switcherCheckbox: HTMLInputElement = document.getElementById('switcher-lang') as HTMLInputElement;
+				switcherCheckbox.checked = true;
+			}
+		},
+		[ langSwitch ]
+	);
 
 	return (
 		<div className="nav-switch lang">
